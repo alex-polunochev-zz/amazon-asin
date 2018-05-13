b90b5c5ab740e0de67b7e9506722036c5bab524f
alex-polunochev <a.volume@gmail.com>
2018-05-12 18:38:53 -0700

    Search form added. Validation, handling submit, waiting state, high-level result recognition

--- src/components/_search_view.js
+++ src/components/_search_view.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 
 export default class SearchView extends React.Component {
 
-  componentDidMount() {
-    let url = 'https://www.amazon.com/dp/B002QYW8LW?th=1';
-    this.cors_api_url = 'https://alex-asin-test.herokuapp.com/';
-    
+  state = {
+    value: '',
+    valid: true,
+    isProcessing: false,
+    foundProductPage: '',
+    searchStatus: ''
+  };
 
-    this.doCORSRequest({
-      method: 'GET',
-      url: url
-    },
-    this.handleAmazonResponse
-    );
+  componentDidMount() {
+    const textInput = this.refs.searchInput;
+
+    if (textInput) {
+      textInput.focus();
+    }
+
+    this.amazonUrl = 'https://www.amazon.com/dp/'; // B002QYW8LW
+    this.cors_api_url = 'https://alex-asin-test.herokuapp.com/';
   }
 
   doCORSRequest = (options, callback) => {
@@ -20,32 +27,104 @@ export default class SearchView extends React.Component {
     x.open(options.method, this.cors_api_url + options.url);
     x.onload = x.onerror = function() {
       callback({status: x.status, response: x.response});
-        // options.method + ' ' + options.url + '\n' +
-        // x.status + ' ' + x.statusText + '\n\n' +
-        // (x.responseText || '')
     };
-    if (/^POST/i.test(options.method)) {
-      x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    }
     x.send(options.data);
   };
 
-  handleAmazonResponse = ({status = -1, response = ''}) => {
-    if(status === 200){
-      console.log("Status 200, parsing...");
-      // parse response here
-    } else {
-      console.log("Response: failure");
+  handleAmazonResponse = ({status = 0, response = ''}) => {
+    let productPage = '';
+
+    if (status === 200){
+      productPage = response;
+    }
+
+    this.setState({isProcessing: false, foundProductPage: productPage, searchStatus: status});
+  };
+
+  handleChange = (event) => {
+    const newValue = event.target.value
+
+    // ASIN is an alphanumeric string up to 10 characters
+    const validInput = newValue.length === 0 || (newValue.length <= 10 && newValue.match(/^[a-z0-9]+$/i));
+
+    this.setState({value: newValue, valid: validInput});
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (this.state.valid && !this.state.isProcessing) {
+      this.setState({isProcessing: true, searchStatus: ''});
+
+      this.doCORSRequest({
+          method: 'GET',
+          url: this.amazonUrl + this.state.value
+        },
+        this.handleAmazonResponse
+      );
     }
   };
 
   render() {
+    const {isProcessing, valid, searchStatus, foundProductPage} = this.state;
+
+    const spinner = isProcessing
+      ? <i className="fa fa-circle-o-notch fa-spin"></i>
+      : '';
+
+    const inputClasses = classNames({
+      "input": true,
+      "input--invalid": !valid,
+      "input--waiting": isProcessing,
+      "input--disabled": isProcessing
+    });
+
+    const searchIconClasses = classNames({
+      "fa": true,
+      "fa-search": true,
+      "fa-search--disabled": isProcessing || !valid,
+      "fa-search--waiting": isProcessing
+    });
+
+    const invalidInputMessage = (
+      <div className="search-form-smallFont">
+        {valid ? '' : '* Amazon Standard Identification Numbers (ASINs) are unique blocks of 10 letters and/or numbers that identify items.'}
+      </div>
+    );
+
+    const notFound = searchStatus && searchStatus !== 200
+     ? <div>Product not found</div>
+     : '';
+
+    const foundProductDetails = searchStatus === 200 && foundProductPage
+      ? <div>Product found </div>
+      : '';
+
     return (
       <div className='search-container'>
         <div className="paddedContainerHeader">
           <h2>Enter ASIN to Find product</h2>
           <div className='paddedContainerBody'>
-            Here goes the form
+            <form onSubmit={this.handleSubmit}>
+              <div className="search-form">
+                <input
+                  type="text"
+                  ref="searchInput"
+                  placeholder="Type ASIN here"
+                  className={inputClasses}
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                  disabled = {isProcessing} />
+
+                <div className={searchIconClasses} title='Submit search' onClick={this.handleSubmit} />
+              </div>
+              {invalidInputMessage}
+            </form>
+            <div className="search-container-results">
+              {spinner}
+              {notFound}
+              {foundProductDetails}
+            </div>
           </div>
         </div>
       </div>
